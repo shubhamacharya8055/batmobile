6d1e5b7ab201b9894e13fb8463ef80f78fc19aab
shubham <essay8055@gmail.com>
2024-06-26 17:43:58 +0530

    fixed: some styles

--- components/Partners.js
+++ components/Partners.js
@@ -6,22 +6,35 @@ import { useEffect, useRef, useState } from "react";
 export default function Partners() {
   const containerRef = useRef(null);
   const [isScrolling, setIsScrolling] = useState(true);
-  const [scrollDirection, setScrollDirection] = useState(1); 
+  const [scrollDirection, setScrollDirection] = useState(1);
+  const [containerWidth, setContainerWidth] = useState(null);
+
+  useEffect(() => {
+    const updateContainerWidth = () => {
+      if (containerRef.current) {
+        setContainerWidth(containerRef.current.clientWidth);
+      }
+    };
+
+    updateContainerWidth(); // Initial update
+    window.addEventListener("resize", updateContainerWidth);
+    return () => window.removeEventListener("resize", updateContainerWidth);
+  }, []);
 
   useEffect(() => {
     let scrollInterval;
 
     function startAutoScroll() {
       scrollInterval = setInterval(() => {
-        if (containerRef.current && isScrolling) {
-          const { scrollLeft, scrollWidth, clientWidth } = containerRef.current;
+        if (containerRef.current && isScrolling && containerWidth) {
+          const { scrollLeft, scrollWidth } = containerRef.current;
           const scrollAmount = 10 * scrollDirection;
 
-          const atEnd = scrollLeft + clientWidth >= scrollWidth && scrollDirection === 1;
+          const atEnd = scrollLeft + containerWidth >= scrollWidth - 1 && scrollDirection === 1; 
           const atStart = scrollLeft === 0 && scrollDirection === -1;
 
           if (atEnd || atStart) {
-            setScrollDirection(scrollDirection * -1); 
+            setScrollDirection(scrollDirection * -1);
           } else {
             containerRef.current.scrollTo({
               left: scrollLeft + scrollAmount,
@@ -34,7 +47,7 @@ export default function Partners() {
 
     startAutoScroll();
     return () => clearInterval(scrollInterval);
-  }, [isScrolling, scrollDirection]); 
+  }, [isScrolling, scrollDirection, containerWidth]); 
 
   const handleMouseEnter = () => setIsScrolling(false);
   const handleMouseLeave = () => setIsScrolling(true);
@@ -45,9 +58,9 @@ export default function Partners() {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div 
+      <div
         ref={containerRef}
-        className="h-fit mx-auto xl:w-[80%] flex gap-x-5 items-center xl:my-10 my-8 px-5 overflow-x-scroll scroll-smooth no-scrollbar"
+        className="h-fit mx-auto xl:w-[80%] w-full flex gap-x-5 items-center xl:my-10 my-8 px-5 overflow-x-scroll scroll-smooth no-scrollbar"
       >
         {PARTNERS.map((partner, index) => (
           <div 
@@ -67,3 +80,4 @@ export default function Partners() {
     </div>
   );
 }
+

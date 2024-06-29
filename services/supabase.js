79f58dd713c1605f5f28afbd8c30210c79295400
shubham <essay8055@gmail.com>
2024-06-29 18:38:39 +0530

    made some minor changes

--- services/supabase.js
+++ services/supabase.js
@@ -5,4 +5,4 @@ export const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-export default supabase
+export default supabase 

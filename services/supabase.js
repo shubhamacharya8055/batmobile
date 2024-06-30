4f4a5d74df489e1a8d10fa5d2cee49e2075c6a26
shubham <essay8055@gmail.com>
2024-06-30 08:57:49 +0530

    fixed: some minor changes in .env and supabase

--- services/supabase.js
+++ services/supabase.js
@@ -1,7 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv';
+dotenv.config(); 
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+
+// if (!supabaseUrl || !supabaseKey) {
+//     throw new Error('Missing Supabase environment variables');
+// }
+  
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase 
